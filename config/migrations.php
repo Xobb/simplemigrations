890396376b76213c3d36b28a1aa312c7faf57c4d
Paul Chubatyy <xobb@citylance.biz>
2011-01-09 15:14:30 +0200

    Added migrations configuration file.

---
+++ config/migrations.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Migration configuration
+ */
+
+return array(
+	'default' => array(
+		/**
+		 * The database group to run migrations on
+		 */
+		'database' => 'default',
+		/**
+		 * The file that migration uses to store database versions.
+		 * It will be located in APPPATH.migrations/ directory.
+		 */
+		'info_file' => '.version',
+	)
+);
